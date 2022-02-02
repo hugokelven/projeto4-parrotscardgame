3e0fa0b8085be0ce7754776095aeb466ec0f4443
hugokelven <hugokelven98@gmail.com>
2022-02-02 03:56:24 -0300

    adicionado a insercao de cartas viradas para baixo

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-selecionarJogo()
+let numeroDeCartas = null
 
 function selecionarJogo() {
-    let numeroDeCartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)")
+    numeroDeCartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)")
 
     while (numeroDeCartas > 14 || numeroDeCartas < 4 || numeroDeCartas%2 !== 0) {
         numeroDeCartas = prompt("Quantas cartas você deseja? (números pares de 4 a 14)")
     }
 }
+
+selecionarJogo()
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function gerarCartas() {
+    const gifs = document.querySelectorAll(".gifs img")
+    let main = document.querySelector("main")
+    let card = document.querySelector(".card")
+    let cardBack = document.querySelector(".card-back")
+
+    main.innerHTML = ""
+
+    let cards = []
+
+    for (let i = 0; i < numeroDeCartas/2; i++) {
+        cardBack.innerHTML = ""
+        cardBack.appendChild(gifs[i])
+        main.appendChild(card.cloneNode(true))
+        main.appendChild(card.cloneNode(true))
+    }
+
+    let newMain = document.querySelectorAll(".card")
+    newMain = Array.from(newMain)
+    newMain.sort(comparador)
+
+    main.innerHTML = ""
+
+    for (let j = 0; j < numeroDeCartas; j++) {
+        main.appendChild(newMain[j].cloneNode(true))
+    }
+}
+
+gerarCartas()
